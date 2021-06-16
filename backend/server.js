58e9e82e4c9734eb834a16243f2d7f82e3a29a12
gustavo.pertuzat <gustavo.pertuzat@hes-so.ch>
2021-06-16 22:06:07 +0200

    feaw

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-let app = express()
-app.use(express.static('../frontend'))
+let app = express();
+app.use(express.static('../frontend'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const meals = require('./modules/db.js');
+
+console.log("server response: " + meals.getBestMeals("test@hesge.ch"));
+
+app.post('/', (req, res) => {
+    res.json(req.body);
+  });
 
 app.listen(8080);
-console.log('Server started')
+console.log('Server started');
